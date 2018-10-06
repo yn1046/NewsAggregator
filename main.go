4d868a1c1e73a6a67c6eb9f26c9810f2dd3ba488
yn1046 <wantedblood@2ch.hk>
2018-10-06 19:08:49 +0300

    added frontend packages

--- main.go
+++ main.go
@@ -5,26 +5,40 @@ import (
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
-func main() {
+// FetchParseHabr is a method that retrieves and parses posts from habr.com
+func FetchParseHabr() {
 	res, err := http.Get("https://habr.com")
 	if err != nil {
-		panic("sasee")
+		panic("Couldn't reach habr.com.")
 	}
 	defer res.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal("SHYTE SUXX")
+		log.Fatal("Couldn't create document from response.")
 	}
 
 	doc.Find(".post_preview").Each(func(i int, s *goquery.Selection) {
 		title := s.Find(".post__title_link").Text()
 		text := s.Find(".post__text.post__text-html").Text()
 		println(title)
-		println(text)
-		println("post found!")
+		println(text + "\n\n\n")
 
 	})
+}
+
+func main() {
+	//FetchParseHabr()
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.File("/", "client/index.html")
+
 }
